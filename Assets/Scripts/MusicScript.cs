22ba603e39a997fab0a8971ae6251d8f302ea973
Enzo Martinez <killerbackofheart@gmail.com>
2025-02-08 23:16:57 +0100

    Add exorciste vfx

--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -3,7 +3,13 @@ using UnityEngine;
 public class MusicScript : MonoBehaviour
 {
     public static MusicScript instance;
+    [Header("Menu SFX")]
     public AudioClip BoutonMenuSFX;
+
+    [Header("Quand on casse les yeux")]
+    public AudioClip ExorcistSFX1;
+    public AudioClip ExorcistSFX2;
+    public AudioClip ExorcistSFX3;
 
     private AudioSource audioSource;
 
@@ -23,6 +29,22 @@ public class MusicScript : MonoBehaviour
     public void PlayBoutonMenuSFX()
     {
         audioSource.clip = BoutonMenuSFX;
+        audioSource.Play();
+    }
+
+    public void PlayExorcistSFX()
+    {
+        int rand = Mathf.FloorToInt(Random.Range(0, 3));
+        if (rand == 0)
+        {
+            audioSource.clip = ExorcistSFX1;
+        } else if (rand == 1)
+        {
+            audioSource.clip = ExorcistSFX2;
+        } else
+        {
+            audioSource.clip = ExorcistSFX3;
+        }
         audioSource.Play();
     }
 }
